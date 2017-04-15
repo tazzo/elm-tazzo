cd6d6a5a991b53ca4fa9f886cd4df792ed60e313
tazzo <roberto.tazzoli@gmail.com>
2017-04-15 02:11:10 +0200

    some comments and old stuff removed

--- src/Main.elm
+++ src/Main.elm
@@ -121,24 +121,6 @@ viewCounter model =
     div
         [ style [ ( "padding", "2rem" ) ] ]
         [ text ("Current count: " ++ toString model.count)
-          {- We construct the instances of the Button component that we need, one
-             for the increase button, one for the reset button. First, the increase
-             button. The first three arguments are:
-
-               - A Msg constructor (`Mdl`), lifting Mdl messages to the Msg type.
-               - An instance id (the `[0]`). Every component that uses the same model
-                 collection (model.mdl in this file) must have a distinct instance id.
-               - A reference to the elm-mdl model collection (`model.mdl`).
-
-             Notice that we do not have to add fields for the increase and reset buttons
-             separately to our model; and we did not have to add to our update messages
-             to handle their internal events.
-
-             Mdl components are configured with `Options`, similar to `Html.Attributes`.
-             The `Button.onClick Increase` option instructs the button to send the `Increase`
-             message when clicked. The `css ...` option adds CSS styling to the button.
-             See `Material.Options` for details on options.
-          -}
         , Button.render Mdl
             [ 0 ]
             model.mdl
